fd369ff7c6869439cfd0de6fab5ad0c36478f2b1
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-22 12:54:46 -0800

    Update datepicker1.py

--- datepicker1.py
+++ datepicker1.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 import selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
@@ -12,9 +10,16 @@ driver.get("https://jqueryui.com/datepicker/")
 driver.maximize_window()
 
 driver.switch_to.frame(0)	  # switch to frame
+datepicker = driver.find_element(By.XPATH, "//input[@id='datepicker']")
 
+datepicker.send_keys("12/22/2022")		# MM/DD/YYYY
+
+year="2023"
+month="January"
+date="23"
+
+datepicker.click()	# open datepicker
 
 ...
-
 
 driver.close()
