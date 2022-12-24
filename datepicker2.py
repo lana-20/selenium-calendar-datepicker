9473bcf70a68f6b37bce2dc30d7c852b81d21ad9
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-24 15:47:41 -0800

    Update datepicker2.py

--- datepicker2.py
+++ datepicker2.py
@@ -8,6 +8,15 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
 driver.maximize_window()
 
+# Date of birth
+driver.find_element(By.XPATH, "//input[@id='dob']").click()	# opens datepicker
+
+datepicker_month = Select(driver.find_element(By.XPATH, "//*[@class='ui-datepicker-month']"))
+datepicker_month.select_by_visible_text("Dec")
+
+datepicker_year = Select(driver.find_element(By.XPATH, "//*[@class='ui-datepicker-year']"))
+datepicker_year.select_by_visible_text("2022")
+
 ...
 
 driver.close()
